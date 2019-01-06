22d5cac567668e039d9faf4e4f15e65d2cd9db71
yriasykioti <44656185+yriasykioti@users.noreply.github.com>
2019-01-06 20:04:50 +0200

    corrected some thing about the use of the primary key

--- Edit.java
+++ Edit.java
@@ -1,4 +1,5 @@
-package Javatoi;
+ppackage Javatoi;
+
 import java.util.*;
 import java.util.LinkedList;
 
@@ -16,7 +17,17 @@ public class Edit {
 		System.out.println("Edit specific data");
 
 		//user chooses the specific data
-		int listIndex = searchfield.searchFieldName(listOfLists);
+
+		System.out.println("In which field is the element you want to change? Options are: ");
+		for(int field=0; field<listOfLists.size();field++) {
+			System.out.printf("%s \n",listOfLists.get(field).get(0));
+		}
+		
+		System.out.println("Enter one of the choices above: ");
+		String fieldName = input.next();
+		int listIndex = searchfield.searchFieldName(listOfLists,fieldName); /*added field name as a parameter so that when 
+		want to view a field it doesnt show ".... you want to change?"*/
+		
 		int lineIndex = searchfield.searchSpecificData(listOfLists, listIndex);
 		//an listIndex == -1 den exei brei primary key
 		//an lineIndex == -1 tote eite den exei brei primary key eite den exei brei element
@@ -27,7 +38,7 @@ public class Edit {
 			removefield.removeSpecificData(listIndex, lineIndex, listOfLists);
 
 			//specific data is added
-			System.out.println("Enter the name of the new element");
+			System.out.println("Enter the new element");
 			String element_name = input.next();
 			addfield.addSpecificData(element_name, listIndex, lineIndex, listOfLists);
 		} else {
